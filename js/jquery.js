db27cc873911032a95e67faac406bd020550905d
MustNotSleep <krossoll@gmail.com>
2019-08-02 15:04:19 -0400

    Update jquery.js

--- js/jquery.js
+++ js/jquery.js
@@ -1,1 +1,1 @@
-$('#painter').click(function(){ MyFunction(); return false; });
+$('#painter').click(function(){ document.getElementByID('painter_link').src='/images/ndiwidn.jpg'"; return false; });
