ec057639221c65b088bd7173d09ba100472b7470
MustNotSleep <krossoll@gmail.com>
2020-02-27 15:49:07 -0500

    Update navigation.js

--- js/navigation.js
+++ js/navigation.js
@@ -2,7 +2,7 @@ function loadNav() {
 
   var pages = ["painting"];
   
-  document.getElementById("page1").href = "/".concat(pages.[0].concat("/"));
+  document.getElementById("page1").href = "/paint";
   document.getElementById("page1").innerHTML = pages.[0];
 }
 
