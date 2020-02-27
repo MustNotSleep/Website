93fe2a2d3e0bf9aaf92875bc743147635f65fbd2
MustNotSleep <krossoll@gmail.com>
2020-02-27 13:08:09 -0500

    Update random.js

--- submitted/random.js
+++ submitted/random.js
@@ -3,5 +3,5 @@ function randomImage() {
   var randomNum = Math.floor(Math.random()*6)+1;
   var randomStr = randomNum.toString();
   var elem = document.getElementById("para1");
-  document.getElementById("para1").innerHTML = randomStr;
+  document.getElementById("para1").innerHTML = "/images/".concat(randomStr.concat(".jpg"));
 }
