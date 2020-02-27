fb9c47db3903850a567264c0b85334572e1891c0
MustNotSleep <krossoll@gmail.com>
2020-02-27 15:24:30 -0500

    Update navigation.js

--- js/navigation.js
+++ js/navigation.js
@@ -1,1 +1,17 @@
+var pages = [
+  
+  { 
+    "name":"painting", 
+    "url":"/painting/" 
+  },
+  { 
+    "name":"drawing", 
+    "url":"/drawing/" 
+  },
+  { 
+    "name":"contact", 
+    "url":"/contact/" },
+  ]
+}
 
+];
