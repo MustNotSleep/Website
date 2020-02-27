e4251e54dab8d3cb10b246dbe025e06b07ff3447
MustNotSleep <krossoll@gmail.com>
2020-02-27 12:49:01 -0500

    Update random.js

--- submitted/random.js
+++ submitted/random.js
@@ -2,5 +2,5 @@ function randomImage() {
 
   var elem = document.getElementById("para1");
   var randomNum = Math.ceiling(Math.random()*6);
-  document.getElementById("para1").innerHTML = randomNum;
+  document.getElementById("para1").innerHTML = randomNum.toString();
 }
