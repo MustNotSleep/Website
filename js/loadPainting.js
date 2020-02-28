984653673389e1ad6d4a9466f4edb2a217d7190f
MustNotSleep <krossoll@gmail.com>
2020-02-28 15:35:07 -0500

    Update loadPainting.js

--- js/loadPainting.js
+++ js/loadPainting.js
@@ -2,13 +2,17 @@ window.addEventListener('load', loadPage, false);
 
 function loadPage() {
 
-  var images = ["/images/25BFF15E-EBE2-493F-B200-2BF65D27AA2B.jpeg"];
-  var captions = ["Still life"];
-  var alt = ["Still life painting"];
-    
+  //image objects consisting of image location, caption, and alt text
+  var images = [
+    ["/images/25BFF15E-EBE2-493F-B200-2BF65D27AA2B.jpeg", "Still life", "Still life painting"]
+  ];
+  
   for (i = 0; i < images.length; i++) {
-   document.getElementById("img".concat((i+1).toString())).src = images[i];
-   document.getElementById("img".concat((i+1).toString())).alt = alt[i];
-   document.getElementById("capt".concat((i+1).toString())).innerHTML = captions[i];
+   //set image location
+   document.getElementById("img".concat((i+1).toString())).src = images[i][0];
+   //set caption
+   document.getElementById("capt".concat((i+1).toString())).innerHTML = images[i][1];
+   //set alt text
+   document.getElementById("img".concat((i+1).toString())).alt = images[i][2];
 }
 }
