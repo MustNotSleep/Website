2317f6eb58340ce0563415b0bb18f8015651b6e8
MustNotSleep <krossoll@gmail.com>
2020-02-28 15:44:56 -0500

    Update navigation.js

--- js/navigation.js
+++ js/navigation.js
@@ -2,6 +2,7 @@ window.addEventListener('load', loadNav, false);
 
 function loadNav() {
 
+  //pages to appear in banner, stored as page name and URL
   var pages = [
     ["painting", "/painting/"], 
     ["drawing", "/drawing/"], 
@@ -10,8 +11,10 @@ function loadNav() {
   
   
   for (i = 0; i < pages.length; i++) {
-    
+  
+  //set page name
   document.getElementById("page".concat((i+1).toString())).innerHTML = pages[i][0];
+  //set URL
   document.getElementById("page".concat((i+1).toString())).href = pages[i][1];
  
   }
