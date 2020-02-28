01ae7f48d0d95ad6ccd5bbc68883df8649416e6c
MustNotSleep <krossoll@gmail.com>
2020-02-28 13:45:13 -0500

    Create loadHomepage.js

---
+++ js/loadHomepage.js
@@ -0,0 +1,15 @@
+window.addEventListener('load', loadPage, false);
+
+function loadPage() {
+
+  var images = ["/images/self-portrait.JPG", "/images/self-portrait.JPG"]
+  var captions = ["Self-portrait, 2019", "test2"]
+  var alt = ["Self-portrait", "test2"]
+  
+  var elem = document.getElementById("img1");
+  
+  
+  for (i = 0; i < images.length; i++) {
+    document.getElementById("img".concat(i.toStr())).src = images[i];
+   document.getElementById("img".concat(i.toStr())).innerHTML = captions[i];
+}
