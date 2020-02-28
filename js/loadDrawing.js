9eded8c04af76ecf4358d041abf4b4c4202d328a
MustNotSleep <krossoll@gmail.com>
2020-02-28 15:33:15 -0500

    Update loadDrawing.js

--- js/loadDrawing.js
+++ js/loadDrawing.js
@@ -2,13 +2,19 @@ window.addEventListener('load', loadPage, false);
 
 function loadPage() {
 
-  var images = ["/images/self-portrait/self-portrait-2019.PNG"];
-  var captions = ["Self-portrait"];
-  var alt = ["Self portrait drawing"];
-    
+  //image objects consisting of image location, caption, and alt text
+  var images = [
+    ["/images/self-portrait/self-portrait-2019.PNG", "Self-portrait", "Self portrait drawing"],
+    ["/images/self-portrait/self-portrait-2019.PNG", "Self-portrait2", "Self portrait drawing2"],
+    ["/images/self-portrait/self-portrait-2019.PNG", "Self-portrait3", "Self portrait drawing3"]
+  ];
+  
   for (i = 0; i < images.length; i++) {
-   document.getElementById("img".concat((i+1).toString())).src = images[i];
-   document.getElementById("img".concat((i+1).toString())).alt = alt[i];
-   document.getElementById("capt".concat((i+1).toString())).innerHTML = captions[i];
+   //set image location
+   document.getElementById("img".concat((i+1).toString())).src = images[i][0];
+   //set caption
+   document.getElementById("capt".concat((i+1).toString())).innerHTML = images[i][1];
+   //set alt text
+   document.getElementById("img".concat((i+1).toString())).alt = images[i][2];
 }
 }
