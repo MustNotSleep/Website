6e352960d49c16acf5cdfea2619753ea7d4149d2
MustNotSleep <krossoll@gmail.com>
2020-03-02 15:30:45 -0500

    Update loadIntroToFig.js

--- js/loadIntroToFig.js
+++ js/loadIntroToFig.js
@@ -35,9 +35,9 @@ function loadIntroToFigure() {
    
   }
   
-   for (i = images.length; i < 26; i++) {
+    for (i = images.length; i < 21; i++) {
+    document.getElementById("if_res".concat((i+1).toString())).style.display = "none";
     document.getElementById("if_img".concat((i+1).toString())).style.display = "none";
-    document.getElementById("if_gal".concat((i+1).toString())).style.padding = "0";
-    document.getElementById("if_gal".concat((i+1).toString())).style.height = "0";
+    document.getElementById("if_gal".concat((i+1).toString())).style.display = "none";
   }
 }
