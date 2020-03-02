945eb71db732a7f4517c65f09ea7eb8509da9252
MustNotSleep <krossoll@gmail.com>
2020-03-02 11:04:29 -0500

    Update loadEducation.js

--- js/loadEducation.js
+++ js/loadEducation.js
@@ -1,4 +1,6 @@
 window.addEventListener('load', loadClassicalFigure, false);
+window.addEventListener('load', loadAtelierDrawing, false);
+window.addEventListener('load', loadIntroToFigure, false);
 
 function loadClassicalFigure() {
 
@@ -15,6 +17,53 @@ function loadClassicalFigure() {
    document.getElementById("cf_img".concat((i+1).toString())).src = images[i][0];
    //set alt text
    document.getElementById("cf_img".concat((i+1).toString())).alt = images[i][1];
+    
+  }
+}
+
+
+function loadAtelierDrawing() {
+
+  //Drawing the Classical Figure
+  //image objects consisting of image location and alt text (no caption)
+  var images = [
+    ["/images/atelier_drawing/1.jpeg", "Cast drawing"],
+    ["/images/atelier_drawing/2.jpeg", "Cast drawing"]
+  ];
+  
+  for (i = 0; i < images.length; i++) {
+  
+   //set image location
+   document.getElementById("ad_img".concat((i+1).toString())).src = images[i][0];
+   //set alt text
+   document.getElementById("ad_img".concat((i+1).toString())).alt = images[i][1];
+    
+  }
+}
+
+
+
+function loadIntroToFigure() {
+
+  //Drawing the Classical Figure
+  //image objects consisting of image location and alt text (no caption)
+  var images = [
+    ["/images/intro_to_fig_draw/2.jpeg", "Figure drawing"],
+    ["/images/intro_to_fig_draw/7.jpeg", "Figure drawing"],
+    ["/images/intro_to_fig_draw/1.jpeg", "Figure drawing"],
+    ["/images/intro_to_fig_draw/18.jpeg", "Figure drawing"],
+    ["/images/intro_to_fig_draw/3.jpeg", "Figure drawing"],
+    ["/images/intro_to_fig_draw/6.jpeg", "Figure drawing"],
+    ["/images/intro_to_fig_draw/9.jpeg", "Figure drawing"],
+    ["/images/intro_to_fig_draw/19.jpeg", "Figure drawing"]
+  ];
+  
+  for (i = 0; i < images.length; i++) {
+  
+   //set image location
+   document.getElementById("if_img".concat((i+1).toString())).src = images[i][0];
+   //set alt text
+   document.getElementById("if_img".concat((i+1).toString())).alt = images[i][1];
     
   }
 }
