0ccb9178f340a66f4ec4c74c3c1613ee08813353
MustNotSleep <krossoll@gmail.com>
2020-03-02 13:43:06 -0500

    Create loadAtelierDrawing.js

---
+++ js/loadAtelierDrawing.js
@@ -0,0 +1,30 @@
+window.addEventListener('load', loadAtelierDrawing, false);
+
+
+function loadClassicalFigure() {
+
+  //Drawing the Classical Figure
+  //image objects consisting of image location and alt text (no caption)
+   var images = [
+    ["/images/atelier_drawing/1.jpeg", "Cast drawing"],
+    ["/images/atelier_drawing/2.jpeg", "Cast drawing"]
+  ];
+  
+  for (i = 0; i < images.length; i++) {
+  
+   //set image location
+   document.getElementById("ad_img".concat((i+1).toString())).src = images[i][0];
+   //set alt text
+   document.getElementById("ad_img".concat((i+1).toString())).alt = images[i][1];
+    
+  }
+
+
+  }
+  
+    for (i = images.length; i < 26; i++) {
+    document.getElementById("if_img".concat((i+1).toString())).style.display = "none";
+    document.getElementById("if_gal".concat((i+1).toString())).style.padding = "0";
+    document.getElementById("if_gal".concat((i+1).toString())).style.height = "0";
+  }
+}
