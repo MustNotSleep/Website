d5264920964de90339eaa25783db4929c705c302
MustNotSleep <krossoll@gmail.com>
2020-03-02 08:45:02 -0500

    Update loadDrawing.js

--- js/loadDrawing.js
+++ js/loadDrawing.js
@@ -1,5 +1,8 @@
 window.addEventListener('load', loadPage, false);
 
+
+function loadPage() {
+
   //image objects consisting of image location, caption, and alt text
   var images = [
     ["/images/self-portrait/self-portrait-2019.PNG", "Self-portrait", "Self portrait drawing"],
@@ -7,10 +10,6 @@ window.addEventListener('load', loadPage, false);
     ["/images/self-portrait/self-portrait-2019.PNG", "Self-portrait3", "Self portrait drawing3"]
   ];
   
-
-
-function loadPage() {
-
   for (i = 0; i < images.length; i++) {
    //set image location
    document.getElementById("img".concat((i+1).toString())).src = images[i][0];
@@ -19,10 +18,6 @@ function loadPage() {
    //set alt text
    document.getElementById("img".concat((i+1).toString())).alt = images[i][2];
     
-  }
-  
-  for (i = images.length; i < 10; i++) {
-    document.getdocument.getElementById("frame".concat((i+1).toString())).setAttribute("padding", 0);
   }
   
 }
