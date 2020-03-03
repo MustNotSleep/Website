be782fbd7d3b6795be29002814d36c3014eec7e6
MustNotSleep <krossoll@gmail.com>
2020-03-03 13:56:57 -0500

    Update loadImage.js

--- js/loadImage.js
+++ js/loadImage.js
@@ -1,13 +1,17 @@
-window.addEventListener('load', loadPage("/images/intro_to_fig_draw/2.jpeg", "Still life", "Still life painting"), false);
+window.addEventListener('load', loadPage, false);
 
-function loadPage(url, caption, alt_text) {
+function loadPage() {
 
- 
+  //image objects consisting of image location, caption, and alt text
+  var images = [
+    ["/images/25BFF15E-EBE2-493F-B200-2BF65D27AA2B.jpeg", "Still life", "Still life painting"]
+  ];
+  
    //set image location
-   document.getElementById("img1").src = url;
+   document.getElementById("img1").src = images[0][0];
    //set caption
-   document.getElementById("capt1").innerHTML = caption;
+   document.getElementById("capt1").innerHTML = images[0][1];
    //set alt text
-   document.getElementById("img1").alt = alt_text;
+   document.getElementById("img1").alt = images[0][2];
 
 }
