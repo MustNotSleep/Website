1d442d2f36741be4814f67fd5b60c92b9a07baf3
MustNotSleep <krossoll@gmail.com>
2020-03-03 14:35:56 -0500

    Delete random.js

--- submitted/random.js
+++
@@ -1,9 +0,0 @@
-function randomImage() {
-
-  var randomNum = Math.floor(Math.random()*6)+1;
-  var randomStr = randomNum.toString();
-  var elem = document.getElementById("randImg");
-  var captions = ["test1", "test2", "test3", "test4", "test5", "test6"]
-  document.getElementById("randImg").src = "/images/submitted/".concat(randomStr.concat(".jpg"));
-  document.getElementById("randCaption").innerHTML = captions[randomNum-1];
-}
