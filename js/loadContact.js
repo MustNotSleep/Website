138d1eae63636881e898f8d685d60e244e6bfc89
MustNotSleep <krossoll@gmail.com>
2020-03-11 13:51:22 -0400

    Update loadContact.js

--- js/loadContact.js
+++ js/loadContact.js
@@ -4,7 +4,9 @@ function loadPage() {
 
   //image objects consisting of image location, caption, and alt text
   var images = [
+    ["/images/self-portrait.JPG", "Self-portrait, 2019", "Self-portrait drawing "]
   ];
+  
   
   for (i = 0; i < images.length; i++) {
    //set image location
