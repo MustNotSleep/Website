97e8b640559ac441616d8882e08b73f7223a08cb
MustNotSleep <krossoll@gmail.com>
2020-05-04 09:31:27 -0400

    Create loadBargue.js

---
+++ js/loadBargue.js
@@ -0,0 +1,24 @@
+window.addEventListener('load', loadPage, false);
+
+function loadPage() {
+
+  //image objects consisting of image location, caption, and alt text
+  var images = [
+    ["/images/bargue/Bargue_1.JPG", "Bargue plate, 2019", "Bargue plate drawing"]
+  ];
+  
+  for (i = 0; i < images.length; i++) {
+   //set image location
+   document.getElementById("img".concat((i+1).toString())).src = images[i][0];
+   //set caption
+   document.getElementById("capt".concat((i+1).toString())).innerHTML = images[i][1];
+   //set alt text
+   document.getElementById("img".concat((i+1).toString())).alt = images[i][2];
+}
+    for (i = images.length; i < 99; i++) {
+  
+      document.getElementById("frame".concat((i+1).toString())).style.padding = "0";
+      document.getElementById("frame".concat((i+1).toString())).style.height = "0";
+    
+  }
+}
