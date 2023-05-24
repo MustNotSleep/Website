c79da44d591ee554e47c08cdc3936a242c67d555
MustNotSleep <krossoll@gmail.com>
2023-05-24 10:20:21 -0400

    Update loadTest.js

--- js/loadTest.js
+++ js/loadTest.js
@@ -24,12 +24,22 @@ function validate() {
 function loadPage(filter_name) {
   
 
-
-var images = [
-        {path: "/images/1.jpg", caption: "Rembrandt caption", alt_text: "Rembrandt, 1615", display_class: "regular", caption_toggle: "hidden_capt", gal_catergory: "portrait"},
-        {path: "/images/2.jpg", caption: "Test caption, 20x40, 2020", alt_text: "", display_class: "regular", caption_toggle: "hidden_capt", gal_catergory: "landscape"},
-        {path: "/images/3.jpg", caption: "", alt_text: "", display_class: "regular", caption_toggle: "hidden_capt", gal_catergory: "portrait"},
-        {path: "/images/4.jpg", caption: "", alt_text: "", display_class: "regular", caption_toggle: "hidden_capt", gal_catergory: "portrait"},
+  /*
+  path = path to file in local directory (e.g. /images/1.kpg
+  caption  = text caption that can be displayed beneath image in either hero or spotlight display
+  alt_text = for screenreaders
+  display_class: either hero (centered, large, top of page, less margin above image than spotlight), spotlight (centered, large, margin all around), or regular (small, displays in grid if screen is large enough)
+  caption_toggle: either visible_capt or hidden_capt - hides or displays the caption. Captions should be hidden if display_class is regular, but may be visible or hidden for other displays
+  gal_category: category the image falls under (e.g. landscape, still_life) - used for filtering images
+  */
+  
+  
+  var images = [
+        {path: "/images/1.jpg", caption: "2022", alt_text: "", display_class: "hero", caption_toggle: "visible_capt", gal_catergory: "still_life"},
+        {path: "/images/2.jpg", caption: "2021", alt_text: "", display_class: "regular", caption_toggle: "hidden_capt", gal_catergory: "still_life"},
+        {path: "/images/3.jpg", caption: "2021", alt_text: "", display_class: "regular", caption_toggle: "hidden_capt", gal_catergory: "still_life"},
+        {path: "/images/4.jpg", caption: "2021", alt_text: "", display_class: "regular", caption_toggle: "hidden_capt", gal_catergory: "still_life"},
+        /*
         {path: "/images/5.jpg", caption: "", alt_text: "", display_class: "regular", caption_toggle: "hidden_capt", gal_catergory: "portrait"},
         {path: "/images/6.jpg", caption: "", alt_text: "", display_class: "regular", caption_toggle: "hidden_capt", gal_catergory: "portrait"},
         {path: "/images/7.jpg", caption: "", alt_text: "", display_class: "regular", caption_toggle: "hidden_capt", gal_catergory: "portrait"},
@@ -62,8 +72,8 @@ var images = [
         {path: "/images/6.jpg", caption: "", alt_text: "", display_class: "regular", caption_toggle: "hidden_capt", gal_catergory: "portrait"},
         {path: "/images/7.jpg", caption: "", alt_text: "", display_class: "regular", caption_toggle: "hidden_capt", gal_catergory: "portrait"},
         {path: "/images/8.jpg", caption: "", alt_text: "", display_class: "regular", caption_toggle: "hidden_capt", gal_catergory: "portrait"},
-      ];
-
+    */
+      ]; 
 
 
 
